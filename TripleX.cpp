a2cc07c8ee55d5668beb6a96ec2bd2ec3e1c48bd
Richard <richard.estes48@gmail.com>
2021-09-07 22:24:37 -0700

    Initial development

---
+++ TripleX.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <ctime>
+
+void PrintIntroduction(int Difficulty, int MaxDifficulty) 
+{
+	std::cout << std::endl;
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNXK00K0kocoddl:,,:ool:,;clc:clxOO0XWMMMMMMMMMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWMMMMMWN0xolc:;;;;;,,',,,',,,',,,'',''',,',,;oKWMMMMMMMMMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMMWXxc,'',,,,,,,,,,,,,,',,,,',''',,,,,,,''lXWMMMMMMMMMMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMWWMMMMMMMMMMMMMMMMMMMMMMMMWNOc,,'',,,,,,,,,,,,,,,,,,,',,',,',,,,,,,l0WMMMMMMMMMMMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWO:',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,l0WWMWWWMWMMMMMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMWWWNNWMMMMMMMMMMMMMMMMMMMMWWXl',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;lxO000KXNWWWWMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMWMMWN0dlOWWWMMMMMMMMMMMMMMMMMMWWO:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;cxOKNWWWMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMNOo;,,oXWWMMMMMMMMMMMMMMMMMWWWO:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'',,''',,',,;lkkk0NWMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMWNkc,,,;dXWWMMMMMMMMMMMMMMMMWWWKc,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;dKWWMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMWWMWXx:,,;oKWMMWWMMMMMMMMWMMMWWWKc,,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',:dONWMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMWKx:,,lOWWWWMWWMMMMMMMMMMWNk:',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;xNWMWMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMWWNKd:,;dKWWMMMMMMMMMMMMWW0l,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;lxXWWWM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMWWWWWWKd;,:xXWWWMMMWWWMMMWKl,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,oXWWW" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMWMMWWWN0o;,ckNWWMWWWWWWWOc,,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,cxXW" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMWMMMMMMWN0o;;oOXNWWWWX0d;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,l0" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWN0l;,:ldxdoc;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,c" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNOl;,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,,,,,',,,:x" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNOl,,,,,,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,,,,,,,,,,,,,,,,,,;oxkOKW" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMWXkc;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,,,,,,,,,',;:::xXWWWMW" << '\n';
+	std::cout << "WWMWNXXXXXKXXXXWWWMMMMMMNXXXXXXXKXWWXXXXKKOo;,,,,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',clc,,,'''',,,,;;,;lOKKXWWWWMMM" << '\n';
+	std::cout << "WWMWXkdl:::::::dKWWMMMMMN0kdc:cox0NN0Okl:::;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',lkOOOkdodxxddk0KO0XWWWMMMMMMMM" << '\n';;
+	std::cout << "MMMWWWN0l,,,,,,,cONWWWWMWWWXd,lKWMWWWWWk:,,,,':l:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,lO0KXNWWWWWWWWWMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMWWWWWNk;,,,,,,,;l0WWWMWMWWO:dNWMMWWWW0:',,''c00d:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'',;;;cd0XNWWMMMMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:oo;,,,,,,:xXWMMWWWO:dNWMMMMMW0c,'',,c0WN0d;,,,,,'',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;ccoxKWWMMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:xXOl;',',,,l0WWWWWOcdNMMMMMMW0c,,,,'cKWWWXo;;,,,,''',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,dXWMWMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMk:xWWXkc,,,,,,;dKWWWO:dWMMMMMMW0c,,,,,c0WWXx;lko;,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',;lkNMMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMMk:kWWMWKd;,,,,,,ckXWO:dNMMMMMMW0:',,,,:dxdc;,lKN0o;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,,oXWMMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:kWMMMWNOl,,,,,,,l0O:dNMMMMMMW0c',,,':odoc;'oXWWNOl:,',',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,,',cONWMMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:kWMMMMWWXk:,,,,,':l;dNMMMMMMW0c,,,,'cKWWXx;lXWMWWNXOl;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'c0WWWMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:kWMMMMWWWWKd;'',,',,dNMMMMMMW0c,,,,'lKWMWNdoXMWWMWWN0xc,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,lKWWMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:kWMMMMWWMWWNOc,,,',,dNMMMMMMW0c,,,,'lKWMMW0ONWWWWWNkoKXkc,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',dNMMMMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWk:kWWMMWWWMMWWWXx:,,,,xNMMMMMMW0c,,,,,cKWMMWWWWWWWXxl:dWWNXkc,,,,,,'',,,,,,,,,,,,,,,,,,,,,,,,,,,,',,oXWMMMMMMMMM" << '\n';
+	std::cout << "MMWWMWWNx;dNWWWWWMMMMMMMWKo,',dNWMMMMWWO:,,',,c0WWWWWWWX0kl;,:OWWMWWXx:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;dXWWMMMMMMM" << '\n';
+	std::cout << "MMMWNKOd:,:dOKXWMMMMMMMMWWXOc;xWWWMNX0Ol,,,,'',codxxdolc:,',,cKWMMMWWWKx:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,dNWMMMMMMM" << '\n';
+	std::cout << "WWMWX0OkkkkkO0XWMMMMMMMMWWWWXk0WMMMNK0OkkkkkkkkkkkkkkkkkOOkkk0NMWMMWWWWN0o:,,',,',,,,,,,,,,,,,,,,,,,,,,,,,,,,'oXWMMMMMMM" << '\n';
+	std::cout << "MMMMMMMWWWWWMMMMMMMMMMMMWMMWWWWMMMMWWWMMWWWWWWWWWWWWWWWWWWWWWWWWMWWWWWWWWN0o;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,:kXWMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMMMMMMMWWX0kxdddddooddddddddxkk0XNWWWWNOxdlc,',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,',,oXMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMWMMMMMWMMMMMMMMMMWWNKkc',,,';okOOkdol;',,;lkXWWWXKo,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'lKMWWMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNd,,,,,:OWMWWWNN0c,,,,,l0WWWWXd;,,,',,,,,,,,,,,,,,,,,,,,,,,,,',,',;xWMMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNd,,,,,:OWWWWMWWWk;,,,,,oNWWWWXo,,,,,,,,'',,,,,,,,,,,,,,,,,,,,,,,,,oXWWMMWM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,,,,,:OWWWWMWWWO:,,,,'lXWWMMMKl,',',,;:,',,,,,,,,,,,,,,,,,,,,,,,,c0WWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,,,,,:OWWWWWMWNx;,,,',dNWMMMMWKl,,,,,;oxc,,,,,,,,,,,,,,,,,,,,,,,,:kWMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,,',,:OWWWWNK0x:,,,,:dXWWMMMMMWO:,,,,';kKxc,'',,,,,,,,,,,,,,,,,,,,dNMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,',,,,ldoool:;,',cokKNWWMMMMMMWNO:,,,,':ONKx:',,,,,,,,,,,,,,,,,,,,dXWMMMW" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,,,,,;dko;,',,,,;xXWWWMWWMMMMMMWNk;,,,',c0WKl;:,,,,,',,,,,,,,,,,,'cKWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,,,,,:OWXkc,,',,,;oKWWWWWMMMMMWMMNx;,,,,,lOdck0d:,,,,'',,,,,,,,,,cONWWMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd,,,,,:OWWWKo,',,,,,cONWWWMMMMMMMMMNd,,,',;;:kNWN0o;,',,',,,,,,,,,c0WWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd'',,':OWWWWXko:,,,,,;dXWWMMMMMMMMMWXl,,,,';kNWWWWN0o;,,',,,,,,,,,,xWMWMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMWWXd,,,,':OWWWMMWN0l,,,,',cONWMMMWWMWWWWKc',,;xNWMMMMMWN0l;,,,,',,,,'cKWWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWN0c',,,';dKNWWWWWWXd;,,',,;dKNWWWWMWMMWW0c,;dNWMMMMMMMWWNOl;,,,,,',,lKWWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0OxllololldkO0NMWWWWNOoollolodOXWWMMMMMMMWOcdNWWMMMMMMMMMWWNOl;,'',,';kWMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWMWWWNWWWWWWWWWWWWWWWMMWWWWWWWWWWMMMMMMMMMMMWNNMMMMMMMMMMMMMMMWNOl,,,,,,oXWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWMMMMMMMMMMMMMMMMMMMMMMMMWXkc,,,'lKWMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWXkc,,dNMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWXxcdNMMMMW" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMMMMMMWWXKNMMMMM" << '\n';
+	std::cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n";
+	std::cout << "\nNERV: MAGI ACCESS TERMINAL\n";
+	std::cout << "PLEASE ENTER THE CORRECT PASSCODE TO CONTINUE... (" << Difficulty << "/" << MaxDifficulty << ") \n\n";
+	
+}
+
+bool PlayGame(int Difficulty, int MaxDifficulty)
+{
+	
+	PrintIntroduction(Difficulty,MaxDifficulty);
+	
+ 	const int CodeA = rand() % Difficulty + Difficulty;
+	const int CodeB = rand() % Difficulty + Difficulty;
+	const int CodeC = rand() % Difficulty + Difficulty;
+	
+	const int CodeSum = CodeA + CodeB + CodeC;
+	const int CodeProduct = CodeA * CodeB * CodeC;
+	
+    // Print sum and product to the terminal
+    std::cout << '\n';
+    std::cout << "+ There are 3 numbers in the code" << '\n';
+	std::cout << "+ The codes add up to: " << CodeSum << '\n';
+	std::cout << "+ The codes multiply to give: " << CodeProduct << '\n';
+	
+	// Get user input for guess
+	int GuessA, GuessB, GuessC;
+	std::cout << "Enter your guess: ";
+	std::cin >> GuessA >> GuessB >> GuessC;
+	 
+	std::cout << "You entered: " << GuessA << GuessB << GuessC << '\n';
+	
+	int GuessSum = GuessA + GuessB + GuessC;
+	int GuessProduct = GuessA * GuessB * GuessC;
+	
+	// Check if player guess is correct
+	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+	{
+		std::cout << "LEVEL " << Difficulty << " ACCESS GRANTED\n";
+		return true;
+	}
+	else
+	{
+		std::cout << "ACCESS DENIED. TRY AGAIN. \n";
+		return false;
+	}
+}
+
+
+
+int main()
+{	
+	srand(time(NULL)); //creates new random sequence based on time of day
+	int LevelDifficulty = 1;
+	const int MaxDifficulty = 5;
+	
+	while (LevelDifficulty <= MaxDifficulty) // Loop until all levels are complete
+	{
+		bool bLevelComplete = PlayGame(LevelDifficulty, MaxDifficulty); //loop until level is complete
+		std::cin.clear(); //clear any errors
+		std::cin.ignore(); //discards the buffer
+		
+		if (bLevelComplete)
+		{
+			++LevelDifficulty;
+		}
+		
+	}
+	std::cout << "\n**** GOD'S IN HIS HEAVEN. ALL'S RIGHT WITH THE WORLD. ****\n";
+	return 0;
+}
